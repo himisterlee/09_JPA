5bf3f53a641869ada916972f5b8bc8d9bcd75146
[himisterlee] <[lejlej100418@gmail.com]>
2024-07-15 11:27:49 +0900

    chap02 진행중

--- chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section02/column/ColumnMappingTests.java
+++ chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section02/column/ColumnMappingTests.java
@@ -1,12 +1,16 @@
-package com.ohgiraffers.section01.entity;
+package com.ohgiraffers.section02.column;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
 
-public class EntityMappingTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ColumnMappingTests {
 
     private static EntityManagerFactory entityManagerFactory;
     private static EntityManager entityManager;
@@ -31,9 +35,9 @@ public class EntityMappingTests {
         entityManager.close();
     }
 
-    @DisplayName("테이블 만들기 테스트")
+    @DisplayName("컬럼에서 사용하는 속성 테스트")
     @Test
-    public void createTableTest() {
+    public void columnMappingTest() {
 
         //given
         Member member = new Member();
@@ -41,18 +45,27 @@ public class EntityMappingTests {
         member.setMemberId("user01");
         member.setMemberPwd("pass01");
         member.setNickname("홍길동");
-        member.setPhone("010-1234-5678");
+//        member.setPhone("010-1234-5678");
         member.setAddress("서울시 종로구");
         member.setEnrollDate(LocalDate.now());
         member.setMemberRole("ROLE_MEMBER");
         member.setStatus("Y");
 
-//        //when
-//        entityManager.persist(member);
-//
-//        //then
-//        Member foundMember = entityManager.find(Member.class, member.getMemberNo());
-//        assertEquals(member.getMemberNo(), foundMember.getMemberNo());
+        //when
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        try {
+            entityManager.persist(member);
+            entityTransaction.commit();
+        } catch (Exception e) {
+            entityTransaction.rollback();
+            throw new RuntimeException(e);
+        }
+
+        //then
+        Member foundMember = entityManager.find(Member.class, member.getMemberNo());
+        assertEquals(member.getMemberNo(), foundMember.getMemberNo());
 
 
     }
