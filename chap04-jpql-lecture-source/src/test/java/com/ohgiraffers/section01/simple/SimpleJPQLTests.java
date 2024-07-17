91e91be25b3d385f9ae334389133fa7f1218cc74
[himisterlee] <[lejlej100418@gmail.com]>
2024-07-17 15:59:06 +0900

    chap04 완료

--- chap04-jpql-lecture-source/src/test/java/com/ohgiraffers/section01/simple/SimpleJPQLTests.java
+++ chap04-jpql-lecture-source/src/test/java/com/ohgiraffers/section01/simple/SimpleJPQLTests.java
@@ -67,6 +67,8 @@ public class SimpleJPQLTests {
         //when
         String jpql = "select m.menuName from menu_section01 m where m.menuCode = 7";
 
+        // entity 하나를 다룰때는 별칭을 쓰지 않아도 괜찮음
+
         TypedQuery<String> query = entityManager.createQuery(jpql, String.class); // 반환결과를 문자열로 받겠다.
 
         String resultMenuName = query.getSingleResult(); // 결과 반환
---
+++ chap04-jpql-lecture-source/src/test/java/com/ohgiraffers/section03/projection/CategoryInfo.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section03.projection;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class CategoryInfo {
+
+    private int categoryCode;
+
+    private String categoryName;
+}
